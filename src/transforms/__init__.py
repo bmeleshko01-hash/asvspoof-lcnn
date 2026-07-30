dd4795c2b1343247b834f7a8731861c8340bb51b
Bogdan Meleshko <bmeleshko01@gmail.com>
2026-07-31 00:28:17 +0300

    norm 3

--- src/transforms/__init__.py
+++ src/transforms/__init__.py
@@ -1,4 +1,4 @@
-from src.transforms.crop import RandomCrop1D
+from src.transforms.crop import CenterCrop1D
 from src.transforms.pad import RepeatPad1D
 from src.transforms.lfcc import LFCC
 from src.transforms.normalize import Normalize1D
--- src/transforms/crop.py
+++ src/transforms/crop.py
@@ -1,30 +1,14 @@
-import random
-
 from torch import nn
 
-class RandomCrop1D(nn.Module):
-    """
-    Randomly crop a 1D tensor to a specified length.
 
-    Args:
-        crop_length (int): The length of the cropped output tensor.
-    """
-
+class CenterCrop1D(nn.Module):
     def __init__(self, crop_length: int):
         super().__init__()
         self.crop_length = crop_length
 
     def forward(self, x):
-        """
-        Args:
-            x (Tensor): input tensor of shape [channels, samples].
-
-        Returns:
-            x (Tensor): cropped tensor.
-        """
         if x.size(-1) <= self.crop_length:
             return x
 
-        start = random.randint(0, x.size(-1) - self.crop_length)
+        start = (x.size(-1) - self.crop_length) // 2
         return x[..., start:start + self.crop_length]
-    
